103288e870d42e1ee7f59d24ca460ec9aaafa4fe
F2582238 Eberson Lima Spadoni <eberson@bb.com.br>
2023-07-30 17:16:54 -0300

    v1.0.1

--- js/preload.js
+++ js/preload.js
@@ -1,23 +1,19 @@
 // Preload images
-function preloadImages() {
-  const images = [
-    'imgs/pixelizer/Strawberry.png',
-    'imgs/pixelizer/floweyOvrw.png',
-    'imgs/pixelizer/floweyOvrw1.png',
-    'imgs/pixelizer/floweyOvrwD.png',
-    'imgs/pixelizer/celeste.jpeg',
-    'imgs/pixelizer/flowey1.jpeg',
-    'imgs/pixelizer/flowey2.jpg',
-    'imgs/bttn/mundo64.gif',
-    'imgs/bttn/wanwan.png'
-  ];
+const images = [
+  "imgs/pixelizer/Strawberry.png",
+  "imgs/pixelizer/floweyOvrw.png",
+  "imgs/pixelizer/floweyOvrw1.png",
+  "imgs/pixelizer/floweyOvrwD.png",
+  "imgs/pixelizer/celeste.jpeg",
+  "imgs/pixelizer/flowey1.jpeg",
+  "imgs/pixelizer/flowey2.jpg",
+  "imgs/bttn/mundo64.gif",
+  "imgs/bttn/wanwan.png",
+];
 
-  for (const image of images) {
-    const img = new Image();
-    img.src = image;
-  }
+for (const image of images) {
+  const img = new Image();
+  img.src = image;
 }
-
 // Call the preloadImages function when the page loads
-window.addEventListener('load', preloadImages);
-console.log('preload finished.')
+console.log("preload finished.");
